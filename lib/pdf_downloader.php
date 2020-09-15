4b7e78c969599c1354dca34ec13bfe0171aebe5c
Federico Cantini <federico.cantini@lib4ri.ch>
2020-09-15 15:23:55 +0200

    Added Elsevier fetching

--- lib/pdf_downloader.php
+++ lib/pdf_downloader.php
@@ -8,6 +8,7 @@ class PdfDownloader{
     private $pdf_url;
     private $doi_resolver = 'http://dx.doi.org/';
     private $locators = ['locateFromMetaCitationPdfUrl', 'locateFromJsonElsevier'];
+    private $pdf;
     
     public function __construct(){
         $this->dom = new DOMDocument();
@@ -28,7 +29,8 @@ class PdfDownloader{
     }
     
     public function fetchHtml(){
-        $this->html = file_get_contents($this->getDoiResolver().$this->doi);
+        $url = $this->getDoiResolver().$this->doi;
+        $this->html = file_get_contents($url);
         $this->dom->loadHTML($this->html);
         return $this;
     }
@@ -50,9 +52,14 @@ class PdfDownloader{
     public function getPdfUrl(){
         return $this->pdf_url;
     }
+            
+    public function fetchPdf(){
+        $this->pdf = file_get_contents($this->pdf_url);
+        return $this;
+    }
     
-    public function setPdfName(){
-
+    public function getPdf(){
+        return $this->pdf;
     }
     
     private function locateFromMetaCitationPdfUrl(){
@@ -67,13 +74,57 @@ class PdfDownloader{
     }
     
     private function locateFromJsonElsevier(){
-        $tags = $this->dom->getElementsByTagName('script');
+        $else_dom = new DOMDocument();
+        $found = false;
+        // step 1 
+        $tags = $this->dom->getElementsByTagName('input');
+        foreach( $tags as $tag ) {
+            if (($tag->getAttribute('name') == 'redirectURL')){
+                $else_url = urldecode($tag->getAttribute('value'));
+                $found = true;
+                break;
+            }
+        }
+        if (!$found){
+            return false;
+        }
+        
+        //step2
+        $found = false;
+        $else_html = file_get_contents($else_url);
+        $else_dom->loadHTML($else_html);
+        
+        $tags = $else_dom->getElementsByTagName('script');
         foreach( $tags as $tag ) {
             if (($tag->getAttribute('type') == 'application/json') && ($tag->getAttribute('data-iso-key') == '_0')){
                 $data = json_decode($tag->nodeValue);
+                $else_url = 'https://www.sciencedirect.com/'.$data->article->pdfDownload->linkToPdf;//.'&isDTMRedir=true&download=true';
+                $found = true;
+                break;
+            }
+        }
+        if (!$found){
+            return false;
+        }
+        
+        //step 3
+        $found = false;
+        $else_html = file_get_contents($else_url);
+        $else_dom->loadHTML($else_html);
+        
+        $tags = $else_dom->getElementsByTagName('div');
+        foreach( $tags as $tag ) {
+            if (($tag->getAttribute('id') == 'redirect-message')){
+                $this->pdf_url = $tag->getElementsByTagName('a')->item(0)->getAttribute('href');
                 return true;
             }
         }
         return false;
     }
+    
+    //convenient function
+    public function returnPdf($doi){
+        return $this->setDoi($doi)->fetchHtml()->locatePdfUrl()->fetchPdf()->getPdf();
+    }
+    
 }
--- test/test.php
+++ test/test.php
@@ -8,4 +8,18 @@ $doi = '10.1016/j.combustflame.2020.08.040';
 
 $down = new PdfDownloader();
 
+// $down->setDoi($doi)->fetchHtml();
+
+// $down->locatePdfUrl();
+
+
+
+
+//$pdf = $down->returnPdf($doi);
+
 $down->setDoi($doi)->fetchHtml()->locatePdfUrl();
+
+
+$down->fetchPdf();
+//echo $down->getPdf();
+file_put_contents("pub.pdf", $down->getPdf());
